26897974e3002680c4fe86abaa67150f8a2f6e03
HEI YEUNG SZE <hsze6@my.bcit.ca>
2024-12-04 14:51:17 -0800

    Add docstrings and doctests to narrative.py

--- narrative.py
+++ narrative.py
@@ -1,4 +1,23 @@
-def print_instructions(game_loop):
+def print_instructions(game_loop: int):
+    """
+    Print respective instructions based on the round of game.
+
+    :param game_loop: an integer
+    :precondition: game_loop must be a positive integer
+    :postcondition: print respective statement according to the integer, representing the round of
+                    game, passed to the function.
+
+    >>> print_instructions(1)
+    <BLANKLINE>
+    You wake up in the forest and are surrounded by beasts. A giant dragon is at the center of the forest. You are forced to involve in endless combat.
+    And there is only one way out!
+
+    >>> print_instructions(2)
+    <BLANKLINE>
+    You can't help falling asleep after you beat the dragon. After a short while...
+    Again, you wake up at same place in the forest and are surrounded by beasts. The giant dragon is still alive at the center of the forest. You are forced to involve in endless combat again.
+    Remember, there is only one way out!
+    """
     if game_loop == 1:
         print("\nYou wake up in the forest and are surrounded by beasts. A giant dragon is at the "
               "center of the forest. You are forced to involve in endless combat.\n"
